0ece53b1d0e99bb5749c5090acd6b45080bf27d3
Henryk Kubiczak <henryk.kubiczak@atendesoftware.pl>
2020-03-24 20:13:52 +0100

    add sample implementation now playing

---
+++ app/src/main/kotlin/com/endeline/mymovie/fragments/nowplaying/NowPlayingAdapter.kt
@@ -0,0 +1,46 @@
+package com.endeline.mymovie.fragments.nowplaying
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.endeline.domain.uimodels.MovieCollectionItemUiModel
+import com.endeline.mymovie.databinding.NowPlayingItemBinding
+import com.endeline.mymovie.extensions.loadLandscapeImage
+
+class NowPlayingAdapter(
+    private val movieCollection: List<MovieCollectionItemUiModel>?
+) : RecyclerView.Adapter<NowPlayingAdapter.FavoriteItemHolder>() {
+
+    class FavoriteItemHolder(view: NowPlayingItemBinding) : RecyclerView.ViewHolder(view.root) {
+        val image = view.imageView
+        val title = view.title
+        val description = view.description
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteItemHolder {
+        return FavoriteItemHolder(NowPlayingItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun getItemCount(): Int {
+        return movieCollection?.size ?: 0
+    }
+
+    override fun onBindViewHolder(holder: FavoriteItemHolder, position: Int) {
+        val movieUiModel = movieCollection!![position]
+
+        with(holder) {
+            title.text = movieUiModel.title
+            description.text = movieUiModel.overview
+
+            movieUiModel.backdropPath?.let {
+                image.loadLandscapeImage(it)
+            }
+
+            itemView.setOnClickListener {
+                it.findNavController().navigate(NowPlayingFragmentDirections.toDetails())
+            }
+        }
+    }
+
+}
--- app/src/main/kotlin/com/endeline/mymovie/fragments/nowplaying/NowPlayingFragment.kt
+++ app/src/main/kotlin/com/endeline/mymovie/fragments/nowplaying/NowPlayingFragment.kt
@@ -1,10 +1,13 @@
-package com.endeline.mymovie.fragments
+package com.endeline.mymovie.fragments.nowplaying
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.endeline.mymovie.databinding.NowPlayingFragmentBinding
 import com.endeline.mymovie.di.components.DaggerViewModelComponent
 import com.endeline.mymovie.viewmodels.NowPlayingViewModel
@@ -19,6 +22,9 @@ class NowPlayingFragment : Fragment() {
 
     private lateinit var binding: NowPlayingFragmentBinding
 
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,13 +36,22 @@ class NowPlayingFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("CheckResult")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.getNowPlaying()
             .subscribeOn(Schedulers.io())
-            .subscribe({
-                    Timber.d("$it")
+            .subscribe(
+                {
+                    viewManager = LinearLayoutManager(requireContext())
+                    viewAdapter = NowPlayingAdapter(it.results)
+
+                    binding.recycleView.apply {
+                        setHasFixedSize(true)
+                        layoutManager = viewManager
+                        adapter = viewAdapter
+                    }
                 }, Timber::e
             )
     }
